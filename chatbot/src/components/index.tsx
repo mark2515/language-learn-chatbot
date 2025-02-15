bd11a8c5e2b4d8c15b46c0d1df54cd40dc787c50
mark2515 <hekunlong2515@gmail.com>
2025-02-15 03:13:49 -0800

    add completion

--- chatbot/src/components/index.tsx
+++ chatbot/src/components/index.tsx
@@ -4,9 +4,13 @@ import { Textarea } from "@mantine/core";
 
 export const Chat = () => {
   const [prompt, setPrompt] = useState("");
+  const [completion, setCompletion] = useState<string>("");
 
   return (
-    <div>
+    <div className="p-10">
+      <div>
+        {completion}
+      </div>
       <Textarea
       style={{width: '60%'}}
       value={prompt} 
